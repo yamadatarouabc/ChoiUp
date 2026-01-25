7f68d22650b7df2b67c5a3422a3dc4d132db86de
yamadatarouabc <xiuxiu1232aq@gmail.com>
2026-01-26 04:26:59 +0900

    fix: i18nのjaをエラーが出るので一時的にコメントアウト

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.default_locale = :ja
+    # エラーが出るので、一時的に:jaの多言語化をコメントアウト
+    # config.i18n.default_locale = :ja
 
     # Don't generate system test files.
     config.generators.system_tests = nil
