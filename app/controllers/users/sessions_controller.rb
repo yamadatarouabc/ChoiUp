27c48df0b868dca8877f1984f0307bcf62e71cf1
yamadatarouabc <xiuxiu1232aq@gmail.com>
2026-01-26 03:44:06 +0900

    feat: sign_out時に仮のsign_inページにリダイレクトするように編集

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,10 +18,13 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
 end
