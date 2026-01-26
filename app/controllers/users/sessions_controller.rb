3390eafd329c26046e0a848bdf1b648d06478e93
yamadatarouabc <xiuxiu1232aq@gmail.com>
2026-01-26 16:34:09 +0900

    fix: rubocopでユーザー登録機能のlint確認

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,7 +28,7 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     root_path
   end
-  
+
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
