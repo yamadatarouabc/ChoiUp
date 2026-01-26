08e95ae1d6a4df62506f40dc3132bdb43e3c2535
yamadatarouabc <xiuxiu1232aq@gmail.com>
2026-01-26 16:22:31 +0900

    feat: sign_in後のリダイレクト先を設定

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,6 +25,10 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
