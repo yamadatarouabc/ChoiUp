9228df3e99a6d629dd2a153063ac6ef170084965
yamadatarouabc <xiuxiu1232aq@gmail.com>
2026-01-27 01:59:40 +0900

    feat: application_helperのLintを確認

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def flash_class(message_type)
     case message_type.to_s
-    when 'notice', 'success'
+    when "notice", "success"
       "bg-green-100 border border-green-400 text-green-700"
-    when 'alert', 'danger', 'error'
+    when "alert", "danger", "error"
       "bg-red-100 border border-red-400 text-red-700"
-    when 'warning'
+    when "warning"
       "bg-yellow-100 border border-yellow-400 text-yellow-700"
     else
       "bg-blue-100 border border-blue-400 text-blue-700"
