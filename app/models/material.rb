74d453a09970eb7d67f66bb18fc3dd4c14bfb4cd
yamadatarouabc <xiuxiu1232aq@gmail.com>
2026-01-28 03:05:38 +0900

    feat: Materialモデルのバリデーションを追加しmaterialsテーブルのマイグレーションを実施

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,10 @@
 class Material < ApplicationRecord
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :url, presence: true,
+                  format: {
+                    with: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
+                    message: 'は正しい形式で入力してください'
+                  }
+  validates :description, length: { maximum: 5000 }, allow_blank: true
 end
--- db/migrate/20260127123507_create_materials.rb
+++ db/migrate/20260127123507_create_materials.rb
@@ -1,7 +1,13 @@
 class CreateMaterials < ActiveRecord::Migration[7.2]
   def change
     create_table :materials do |t|
+      t.string :title, null: false, limit: 100
+      t.string :url, null: false
+      t.text :description, limit: 65535
+
       t.timestamps
     end
+
+    add_index :materials, :url
   end
 end
